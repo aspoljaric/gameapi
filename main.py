7aacec0b39e73af7b397144f9b2ef56807ea595c
Albert Spoljaric <albert.spoljaric@gmail.com>
2016-10-24 10:29:26 +1100

    feat: CRON job for send email reminder for active games

--- main.py
+++ main.py
@@ -8,20 +8,29 @@ from google.appengine.api import mail, app_identity
 from api import TicTacToeApi
 from utils import get_by_urlsafe
 from models import User, Game
+from google.appengine.ext import ndb
 
 
 class SendReminderEmail(webapp2.RequestHandler):
 
     def get(self):
-        """Send a reminder email to each User with an email about games.
+        """Send a reminder email to each User with an email about games
+        in progress.
         Called every hour using a cron job"""
         app_id = app_identity.get_application_id()
         users = User.query(User.email != None)
+
         for user in users:
-            subject = 'This is a reminder!'
-            body = 'Hello {}, try out Guess A Number!'.format(user.name)
-            # This will send test emails, the arguments to send_mail are:
-            # from, to, subject, body
+            games = Game.query(ndb.OR(Game.user_x == user.key,
+                                      Game.user_o == user.key)). \
+                filter(Game.is_game_over == False). \
+                filter(Game.is_cancelled == False)
+            subject = 'Tic-Tac-Toe: This is a reminder!'
+            body = """Hello {}, You have the following game(s) in progress (keys)
+            - {}""". \
+                format(user.name,
+                       ', '.join(game.key.urlsafe() for game in games))
+
             mail.send_mail('noreply@{}.appspotmail.com'.format(app_id),
                            user.email,
                            subject,
@@ -55,7 +64,7 @@ class MoveNotificationEmail(webapp2.RequestHandler):
                        body)
 
 app = webapp2.WSGIApplication([
-    #('/crons/send_reminder', SendReminderEmail),
+    ('/crons/send_reminder', SendReminderEmail),
     ('/tasks/cache_count_active_games', UpdateCountActiveGames),
     ('/tasks/move_notification_email', MoveNotificationEmail),
 ], debug=True)
